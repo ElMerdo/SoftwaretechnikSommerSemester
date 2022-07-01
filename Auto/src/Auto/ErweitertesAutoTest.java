2009a24588947b1a521df5c49a44a117c8939b3e
ElMerdo <107750719+ElMerdo@users.noreply.github.com>
2022-07-01 22:13:08 +0200

    Tests angepasst und neue Meldung hinzugefuegt

--- Auto/src/Auto/ErweitertesAutoTest.java
+++ Auto/src/Auto/ErweitertesAutoTest.java
@@ -6,24 +6,27 @@ public class ErweitertesAutoTest {
 	public static void main(String[] args) {
 				
 		try {
-			ErweitertesAuto pkw1 = new ErweitertesAuto("Hans2", "SUV", "Grün", 2010, 160, 80000, "Berlin", "S12U34V56");
+			ErweitertesAuto pkw1 = new ErweitertesAuto("Hans2", "SUV", "Grï¿½n", 2010, 160, 80000, "Berlin", "S12U34V56", false);
 			pkw1.meldung();
+			pkw1.MeldungUnfallfahrzeug();
 			System.out.print(". Das Auto ist " + pkw1.getAlter() + " Jahre alt.\n");
 			System.out.println(pkw1.toString());
 			System.out.println("Aktueller Kilometerstand: " + pkw1.getKmStand());
-			pkw1.SetzeNeuesZiel("München", 700);
+			pkw1.SetzeNeuesZiel("Mï¿½nchen", 700);
 			System.out.println("Neuer Kilometerstand: " + pkw1.getKmStand() + " Neuer Standort: " + pkw1.getStandort());
 		} catch (FalscheErstzulassungsAusnahme e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
 		
-		ErweitertesAuto pkw3 = new ErweitertesAuto("Micha2", "Kleinwagen", "Grau", 2017, 70, -20, "Hamburg", "K12L34E56I78N90");
+		ErweitertesAuto pkw3 = new ErweitertesAuto("Micha2", "Kleinwagen", "Grau", 2017, 70, 20, "Hamburg", "K12L34E56I78N90", true);
 		try {
+			System.out.println("\n\n Nun kommt der " + pkw3.getFarbe() + "e " + pkw3.getAutotyp() + " von " + pkw3.getBesitzer());
 			pkw3.meldung();
-			System.out.print(". Das Auto ist " + pkw3.getAlter() + " Jahre alt.\n");
+			System.out.print(". Das Auto ist " + pkw3.getAlter() + " Jahre alt\n");
+			pkw3.MeldungUnfallfahrzeug();
 		} catch (FalscheErstzulassungsAusnahme e) {
-			
+			e.printStackTrace();
 		}
 		System.out.println(pkw3.toString());
 		System.out.println("Aktueller Kilometerstand: " + pkw3.getKmStand());
